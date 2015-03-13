8225640d391040bfcd66892c4377a798cb7687ad
gl0Rob <robgloster@me.com>
2015-03-13 02:32:31 -0400

    Initial commit of front end application. Sync with codeanywhere

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/welcome.json', function(req, res) {
+	res.send('head');
+});
 module.exports = router;
